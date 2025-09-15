a357e0e19a3b947337a24aa6188829fc05b43318
chahe-dridi <chaher.dridi.100@gmail.com>
2025-09-15 14:27:02 +0100

    added event model / method to string str

--- twin3/Event/models.py
+++ twin3/Event/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+category_list = [
+    ('Musique', 'Musique'),
+    ('Cinema', 'Cinema'),
+    ('Sport', 'Sport'),
+]
+
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    
+    image = models.ImageField(upload_to='event_images/', blank=True, null=True)
+    
+    category = models.CharField(choices=category_list, max_length=30 )
+    state = models.BooleanField(default=True)
+   
+    nbe_participant = models.IntegerField(default=0)
+    evt_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+ 
+    def __str__(self):
+        return self.title
